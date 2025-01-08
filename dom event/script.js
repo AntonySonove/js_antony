14e6d13d095bf8296aaa277df51b5a027d83a263
antony <asonove@gmail.com>
2025-01-08 11:52:34 +0100

    save

--- dom event/script.js
+++ dom event/script.js
@@ -18,9 +18,32 @@ truc.addEventListener("click",()=>{
     titre.style.display="block";
 })
 
-const input=document.getElementById("input");
-console.log(input);
+// const input=document.getElementById("input");
+// console.log(input);
 // const inputBlur= input.blur(input.style.backgroundColor="blue");
 
-input.addEventListener("focus",()=>input.style.backgroundColor="blue");
-input.addEventListener("blur",()=>input.style.backgroundColor="white");
+// input.addEventListener("focus",()=>input.style.backgroundColor="blue");
+// input.addEventListener("blur",()=>input.style.backgroundColor="white");
+
+
+// * Mini test pour capter event du Clavier
+// document.addEventListener('keyup', function(eventClavier){
+//     console.log('Yes Moi ECRIRE');
+//     console.log(eventClavier);
+//     console.log(eventClavier.key);
+// });
+
+
+const input=document.getElementById("input");
+const recupTexte=document.getElementById("recupTexte");
+
+input.addEventListener("keyup",(eventClavier)=>recupTexte.innerText+=eventClavier.key);
+
+
+//local storage
+
+// const test=localStorage.setItem("monSuperTexte","pré rempli");
+const newSuperTexte=localStorage.getItem("monSuperTexte");
+input.value=newSuperTexte;
+
+input.addEventListener("keyup",(eventClavier)=>test=localStorage.setItem("monSuperTexte",eventClavier.key));
