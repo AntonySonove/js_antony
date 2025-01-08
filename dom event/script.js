6ba63fc851145e9c1b73bbf12ddeab90febfff2b
antony <asonove@gmail.com>
2025-01-08 15:54:28 +0100

    save

--- dom event/script.js
+++ dom event/script.js
@@ -50,13 +50,14 @@ input.addEventListener("keyup",()=>test=localStorage.setItem("monSuperTexte",inp
 
 
 //vanta
-VANTA.BIRDS({
-    el: "#vanta",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00
-  })
+// VANTA.BIRDS({
+//     el: "#vanta",
+//     mouseControls: true,
+//     touchControls: true,
+//     gyroControls: false,
+//     minHeight: 200.00,
+//     minWidth: 200.00,
+//     scale: 1.00,
+//     scaleMobile: 1.00
+//   })
+
