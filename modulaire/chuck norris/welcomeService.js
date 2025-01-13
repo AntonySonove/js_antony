de4d2cffc74c06113e8a05fdc4381fb9e28f12cc
antony <asonove@gmail.com>
2025-01-13 16:55:49 +0100

    save

--- modulaire/chuck norris/welcomeService.js
+++ modulaire/chuck norris/welcomeService.js
@@ -1,15 +1,15 @@
-const apiDiv=document.getElementById("apiDiv");
-// console.log(apiDiv);
+
 export const contactApi=async()=>{
+    const apiDiv=document.getElementById("apiDiv");
+    // console.log(apiDiv);
     const response=await fetch("https://api.chucknorris.io/jokes/random");
     console.log(response);
-    // console.log(response.ok);
-    // console.log(response.status);
+    console.log(response.ok);
+    console.log(response.status);
 
-    const dataTransformed=await response.json
+    const dataTransformed=await response.json();
     // console.log(dataTransformed);
 
     const blague=document.body.appendChild(document.createElement("h4"));
-    blague.innerText=response.value;
+    blague.innerText=dataTransformed.value;
 }
-
